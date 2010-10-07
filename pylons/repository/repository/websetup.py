7b25a6af8183957e602ee5c2a2eeacd58764823b
Matt Vliet <matt.vliet@gmail.com>
2010-10-07 16:50:07 -0700

    development.ini now points to a file at /home/mvliet/repoman_admins.  Fill this with lines of the form name,email,dn for the database to be populated with when running setup-app

--- pylons/repository/repository/websetup.py
+++ pylons/repository/repository/websetup.py
@@ -8,6 +8,7 @@ from repository.model.meta import Session, Base
 
 from repository import model
 import uuid
+from os import path
 
 log = logging.getLogger(__name__)
 
@@ -27,5 +28,19 @@ def setup_app(command, conf, vars):
     users.uuid = uuid.uuid3(namespace, 'GROUP'+'users').hex
     Session.add(users)
     Session.commit()
+
+    # add some users from a file into the db for testing
+    # each line of the file should be of the form name,email,dn
+    admin_file = conf.global_conf['admin_file']
+    f = open(path.expandvars(admin_file), 'r')
+    for line in f:
+        name, email, dn = line.split(',')
+        user = model.User(name=name, email=email, client_dn=dn)
+        user.uuid = uuid.uuid3(namespace, dn).hex
+        user.gobal_admin=True
+        user.suspended=False
+        Session.add(user)
+        Session.commit()
+    f.close()
 
 
