d053d26f8f7e6459d3f257660894e92275f46005
Matt Vliet <matt.vliet@gmail.com>
2010-11-17 14:09:51 -0800

    client example changes

--- server/repoman.py
+++ server/repoman.py
@@ -3,8 +3,15 @@
 import simplejson
 import httplib
 import urllib
+from os import getuid
 
 HEADERS = {"Content-type":"application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+DEFAULT_CLI_CONFIG = {}
+
+DEFAULT_CLIENT_CONFIG = {'host':'localhost',
+                         'port':443,
+                         'cert':'/tmp/x509up_u%s' % getuid()}
 
 class RepomanError(Exception):
     def __init__(self, e):
@@ -27,7 +34,6 @@ class RepomanClient(object):
 
     Example:
         repo = RepomanClient('repo.server.ca', 443, '/tmp/x509up_u1000')
-        repo.connect()
         # Now connected to a repo.server.ca.  Any of the client commands will now work.
     """
     def __init__(self, cert, host, port):
@@ -35,10 +41,15 @@ class RepomanClient(object):
         self.host = host
         self.port = port
 
-    def connect(self):
+        self._connect()
+
+    def _connect(self):
         self.conn = httplib.HTTPSConnection(host=self.host, port=self.port, cert_file=self.cert)
 
-    def get(self, url):
+        user = self.whoami()
+        self.user_name = user['user_name']
+
+    def _get(self, url):
         try:
             self.conn.request('GET', url)
         except Exception, e:
@@ -46,7 +57,7 @@ class RepomanClient(object):
         else:
             return self.conn.getresponse()
 
-    def post(self, url, params={}, headers=HEADERS):
+    def _post(self, url, params={}, headers=HEADERS):
         try:
             params = urllib.urlencode(params)
             self.conn.request('POST', url, params, headers)
@@ -55,7 +66,7 @@ class RepomanClient(object):
         else:
             return self.conn.getresponse()
 
-    def delete(self, url):
+    def _delete(self, url):
         try:
             self.conn.request('delete', url)
         except Exception, e:
@@ -64,175 +75,204 @@ class RepomanClient(object):
             return self.conn.getresponse()
 
     def whoami(self):
-        resp = self.get('/api/whoami')
+        resp = self._get('/api/whoami')
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def get_all_users(self):
-        resp = self.get('/api/users')
+        resp = self._get('/api/users')
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def get_all_images(self):
-        resp = self.get('/api/images')
+        resp = self._get('/api/images')
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def get_all_groups(self):
-        resp = self.get('/api/groups')
+        resp = self._get('/api/groups')
+        if resp.status == 200:
+            return simplejson.loads(resp.read())
+        else:
+            raise BadResponse(resp)
+
+    def get_user_images(self, user):
+        resp = self._get('/api/users/%s/images' % user)
+        if resp.status == 200:
+            return simplejson.loads(resp.read())
+        else:
+            raise BadResponse(resp)
+
+    def get_my_images(self):
+        return self.get_user_images(self.user_name)
+
+    def get_images_shared_with_user(self, user):
+        resp = self._get('/api/users/%s/shared' % user)
+        if resp.status == 200:
+            return simplejson.loads(resp.read())
+        else:
+            raise BadResponse(resp)
+
+    def get_images_shared_with_me(self):
+        return self.get_images_shared_with_user(self.user_name)
+
+    def get_images_shared_by_me(self):
+        resp = self._get('/api/users/%s/sharing' % self.user_name)
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def get_user(self, user):
-        resp = self.get('/api/users/%s' % user)
+        resp = self._get('/api/users/%s' % user)
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def get_image(self, image):
-        resp = self.get('/api/images/%s' % image)
+        resp = self._get('/api/images/%s' % image)
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def get_group(self, group):
-        resp = self.get('/api/groups/%s' % group)
+        resp = self._get('/api/groups/%s' % group)
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def new_user(self, **kwargs):
-        resp = self.post('/api/users', kwargs)
+        resp = self._post('/api/users', kwargs)
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def new_group(self, name,**kwargs):
-        resp = self.post('/api/groups', kwargs)
+        resp = self._post('/api/groups', kwargs)
         if resp.status == 200:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def new_image(self, **kwargs):
-        resp = self.post('/api/images', kwargs)
+        resp = self._post('/api/images', kwargs)
         if resp.status == 201:
             return simplejson.loads(resp.read())
         else:
             raise BadResponse(resp)
 
     def delete_user(self, name):
-        resp = self.delete('/api/users/%s' % user)
+        resp = self._delete('/api/users/%s' % user)
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def delete_group(self, group):
-        resp = self.delete('/api/groups/%s' % group)
+        resp = self._delete('/api/groups/%s' % group)
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def delete_image(self, image):
-        resp = self.delete('/api/images/%s' % image)
+        resp = self._delete('/api/images/%s' % image)
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def modify_user(self, user, **kwargs):
-        resp = self.post('/api/users/%s' % user, kwargs)
+        resp = self._post('/api/users/%s' % user, kwargs)
         if resp.status == 200:
-            return simplejson.loads(resp.read())
+            return True
         else:
             raise BadResponse(resp)
 
     def modify_image(self, image, **kwargs):
-        resp = self.post('/api/images/%s' % image, kwargs)
+        resp = self._post('/api/images/%s' % image, kwargs)
         if resp.status == 200:
-            return simplejson.loads(resp.read())
+            return True
         else:
             raise BadResponse(resp)
 
     def modify_group(self, group, **kwargs):
-        resp = self.post('/api/groups/%s' % image, kwargs)
+        resp = self._post('/api/groups/%s' % group, kwargs)
         if resp.status == 200:
-            return simplejson.loads(resp.read())
+            return True
         else:
             raise BadResponse(resp)
 
     def add_user_to_group(self, user, group):
-        resp = self.post('/api/groups/%s/users/%s' % (group, user))
+        resp = self._post('/api/groups/%s/users/%s' % (group, user))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def remove_user_from_group(self, user, group):
-        resp = self.delete('/api/groups/%s/users/%s' % (group, user))
+        resp = self._delete('/api/groups/%s/users/%s' % (group, user))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def add_permission(self, group, permission):
-        resp = self.post('/api/groups/%s/permissions/%s' % (group, permission))
+        resp = self._post('/api/groups/%s/permissions/%s' % (group, permission))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def remove_permission(self, group, permission):
-        resp = self.delete('/api/groups/%s/permissions/%s' % (group, permission))
+        resp = self._delete('/api/groups/%s/permissions/%s' % (group, permission))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def share_with_user(self, image, user):
-        resp = self.post('/api/images/%s/share/user/%s' % (image, user))
+        resp = self._post('/api/images/%s/share/user/%s' % (image, user))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def unshare_with_user(self, image, user):
-        resp = self.delete('/api/images/%s/share/user/%s' % (image, user))
+        resp = self._delete('/api/images/%s/share/user/%s' % (image, user))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def share_with_group(self, image, group):
-        resp = self.post('/api/images/%s/share/group/%s' % (image, group))
+        resp = self._post('/api/images/%s/share/group/%s' % (image, group))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def unshare_with_group(self, image, group):
-        resp = self.delete('/api/images/%s/share/group/%s' % (image, group))
+        resp = self._delete('/api/images/%s/share/group/%s' % (image, group))
         if resp.status == 200:
             return True
         else:
             raise BadResponse(resp)
 
     def upload_raw(self, image, raw):
-        url = 'https://' + self.host + ':' + self.port + '/'
+        import pycurl
+
+        url = 'https://' + self.host + ':' + str(self.port)
         url += '/api/images/raw/%s' % image
         c = pycurl.Curl()
         c.setopt(pycurl.POST, 1)
@@ -241,9 +281,17 @@ class RepomanClient(object):
         c.setopt(pycurl.SSL_VERIFYHOST, 0)
         c.setopt(pycurl.SSL_VERIFYPEER, 0)
         c.setopt(pycurl.SSLCERT, self.cert)
-        c.perform()
-        if c.status == 200:
+        try:
+            c.perform()
             return True
-        else:
+        except:
             raise BadResponse(c)
+
+
+class RepomanCLI(object):
+    def __init__(self):
+        pass
+
+    def make_parser(self):
+        pass
 
