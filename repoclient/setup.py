e09356fedf12a39cf725e2aae34efd69ddaa6a7e
Drew Harris <dbharris@uvic.ca>
2010-11-26 13:50:27 -0800

    changed setup script

--- repoclient/setup.py
+++ repoclient/setup.py
@@ -15,23 +15,15 @@ if not os.path.exists(path):
 if os.path.isfile(path + 'repoclient.conf'):
     print os.getenv("HOME") + '/.repoman/repoclient.conf already exists.'
     print "Using this configuration - if you wish to use the default please move this file."
-    setup(name='repoclient',
-        version='0.1',
-        description='Client to connect to VM image repository',
-        author='Kyle Fransham, Drew Harris',
-        author_email='fransham@uvic.ca, dbharris@uvic.ca',
-        url='http://github.com/hep-gc/repoman',
-        packages=['repoclient'],
-        scripts=['repoman'],
-    )
-else:
-    setup(name='repoclient',
-        version='0.1',
-        description='Client to connect to VM image repository',
-        author='Kyle Fransham, Drew Harris',
-        author_email='fransham@uvic.ca, dbharris@uvic.ca',
-        url='http://github.com/hep-gc/repoman',
-        packages=['repoclient'],
-        scripts=['repoman'],
-        data_files=[(path,['conf/repoclient.conf'])]
-    )
+    sys.exit(2)
+
+setup(name='repoclient',
+    version='0.1',
+    description='Client to connect to VM image repository',
+    author='Kyle Fransham, Drew Harris',
+    author_email='fransham@uvic.ca, dbharris@uvic.ca',
+    url='http://github.com/hep-gc/repoman',
+    packages=['repoclient'],
+    scripts=['repoman'],
+    data_files=[(path,['conf/repoclient.conf'])]
+)
